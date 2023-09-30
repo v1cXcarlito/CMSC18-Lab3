947bf27ad7fcbc5f69d5e1898e63746b042b69b4
0xshr00msz <yldeviermagpusao@gmail.com>
2023-10-01 00:38:15 +0800

    Added atol function to display the 0 at the start

--- upc.c
+++ upc.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 // Main Function of the upc.c file
 int main(){
     int firstSum = 0, secondSum = 0, remainder, total;
-    long int upc;
+    char upc_str[12];
     int counter = 0;
 
     printf("Enter the 11-digit Universal Product Code: ");
-    scanf("%ld", &upc);
-    printf("\nUPC: %ld\n", upc);
+    scanf("%s", &upc_str);
+    printf("\nUPC: %s\n", upc_str);
+    long int upc = atol(upc_str);
+    
 
     while(upc != 0){
         remainder = upc % 10;
