ff456c0a9eb0f8754130503be4101a123e53bc49
0xshr00msz <yldeviermagpusao@gmail.com>
2023-10-01 12:43:08 +0800

    Added partial comments

--- upc.c
+++ upc.c
@@ -1,18 +1,23 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<stdlib.h> // This header file is for the atol() function
 
 // Main Function of the upc.c file
 int main(){
+    // Initialize and declare the needed variables
     int firstSum = 0, secondSum = 0, remainder, total;
+    // An array that stores the UPC code: 11 digits + 1 for the null terminator
     char upc_str[12];
-    int counter = 0;
 
-    printf("Enter the 11-digit Universal Product Code: ");
-    scanf("%s", &upc_str);
+    // Enter the 11 digit universal product code.
+    // The program receives the input as string
+    printf("Enter the 11-digit Universal Product Code: ");       
+    scanf("%s", &upc_str);                                     
     printf("\nUPC: %s\n", upc_str);
+    
+    // The atol function is used to convert the character string into a long value
     long int upc = atol(upc_str);
     
-
+    int counter = 0;
     while(upc != 0){
         remainder = upc % 10;
         // firstSum adding odd numbers
